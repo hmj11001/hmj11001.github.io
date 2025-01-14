8a44f9f12e308f7bc2eb707a4c5f925d00c91b20
Hannah Johnson <hannah@Hannahs-Air.cable.rcn.com>
2025-01-14 12:53:52 -0500

    new intro spacing

--- src/components/Section1-intro.js
+++ src/components/Section1-intro.js
@@ -17,18 +17,19 @@ const Section1 = () => {
               </h2>
             </header>
             <p>
-             I hold a Professional Certificate in Full Stack Development from the MIT xPro program, where I mastered the 
-             MERN stack and developed a passion for crafting visually engaging web applications. </p>
-            <p>
-             I earned a BA in Spanish Language and Literature from UConn, which sharpened my communication skills and expanded my global perspective.
-             </p><p>
-             With a diverse background in hospitality, real estate, and cybersecurity, I bring creativity, adaptability, and 
-             problem-solving to every project I work on.</p>
+             I hold a Professional Certificate in Full Stack Development from the MIT xPro program, where I gained experience in the 
+             MERN stack and developed a passion for crafting visually engaging web applications.
+             My Bachelor’s degree in Spanish Language and Literature from UConn honed my communication skills and broadened my 
+             cultural perspective, making it easy to connect with diverse teams and audiences.</p>
+             <p>
+             My career journey has taken me through hospitality, real estate, and cybersecurity, giving me a unique blend of 
+             creativity, adaptability, and problem-solving that I bring to every project. Whether it's designing intuitive 
+             interfaces or tackling complex challenges, I thrive on creating meaningful solutions.
+             </p>
             <p>
             Outside of work, I enjoy scuba diving and strategy board games, both of which offer a chance to think 
             critically and explore new challenges, whether it's underwater or on the game board. 
-            </p><p>
-              I’m excited to apply my skills in a new role where I can continue to grow professionally and contribute to my team’s success.
+            I’m looking forward to applying my skills in a new role where I can continue to grow professionally and contribute to my team’s success.
             </p>
           </div>
           <div className="col-6 col-12-medium imp-medium">
